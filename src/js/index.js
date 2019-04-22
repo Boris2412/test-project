5d64a8922fe9c9f56aad95f4f804239c7f895d41
Boris Bolshakov <boris.bolsh@gmail.com>
2019-04-22 22:38:41 +0300

    form added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
 import jQuery from "jquery";
 
-jQuery(function() {
-  jQuery("body").css("color", "");
+jQuery(document).ready(function() {
+
+    //Range slider
+    const slider = document.querySelector('.range-block__slider');
+    const output = document.querySelector('.range-block__percent');
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    };
+
+
+    //Input type file
+
+
 });
