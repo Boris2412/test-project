bf789516f9bd4599f1018bcbdfc915f2e542bff2
Boris Bolshakov <boris.bolsh@gmail.com>
2019-04-26 21:53:21 +0300

    tablet adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,22 @@
 import jQuery from "jquery";
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-    //Range slider
-    const slider = document.querySelector('.range-block__slider');
-    const output = document.querySelector('.range-block__percent');
-    output.innerHTML = slider.value;
+  //Range slider
+  const slider = document.querySelector('.range-block__slider');
+  const output = document.querySelector('.range-block__percent');
+  output.innerHTML = slider.value;
 
-    slider.oninput = function() {
-      output.innerHTML = this.value;
-    };
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+  };
 
 
-    //Input type file
+  // Mobile menu
+  jQuery(".nav-btn__button").on("click", function () {
+    const target = jQuery(this).data("target");
+    jQuery(target).toggleClass("nav__list--open");
+  });
 
 
 });
